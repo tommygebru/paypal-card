668cca652165fd2d32ca69a4a22887270facf051
tommygebru <gebru.t@gmail.com>
2016-10-20 15:17:32 -0700

    Master:Comment-out javascipt snippets, and include css box-shadow,s for credit-card class.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $(".credit-card").mouseenter(function() {
+/*  $(".credit-card").mouseenter(function() {
     $(this).css({
       "box-shadow": "0px 4px 50px 0px rgba(0,0,0, 1)",
       "transition": "2s"
@@ -10,24 +10,26 @@ $(document).ready(function() {
       "box-shadow": "0px 0px 0px 0px rgba(0,0,0, 1)",
       "transition": "2s"
     });
-  });
+  });*/
 
- 
-  $(function() {
+/*****************************************
+*****************************************/
+
+/*  $(function() {
  $("input").on('keyup', function() {
-   
+
    var logged = $(this).val();
  var str1 = $(this).val().substring(0,4);
    $("#firstfour").html(str1);
    var numint = 0;
    numint+=4;
    console.log(numint);
-   
+
 var fifth = logged.replace(logged.charAt(numint + 1), " ");
    console.log(fifth);
  });
 });
-  
+
   if ($("input").length=17)
     {
       $("#account-number").css({"border":"rgba(0,0,0,0)", "background" : "rgba(0,0,0,0)" });
@@ -35,18 +37,18 @@ var fifth = logged.replace(logged.charAt(numint + 1), " ");
   /*
 $("#account-number").on("input", function(){
   $("#firstfour").text(this.value.substring(0,4));
-  
+
 });
 
 */
-  
 
 
- 
-    
-    
-    
- 
 
-  
+
+
+
+
+
+
+
 });
